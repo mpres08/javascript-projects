263547650818e74caf9e3ffd3ab4ab2e6faedc86
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-06-16 22:45:48 -0500

    Upload completed exercise

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -19,10 +19,16 @@ console.log("launchReady = ", launchReady);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   crewReady = true;
 } else {
    console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
+   crewReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("crewReady = ", crewReady);
+
+if (crewReady && launchReady === true){
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed");
+}

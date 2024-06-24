98896ca6dc2284b71bf3822c7342a00115093052
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-06-23 19:42:47 -0500

    Upload files

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -4,17 +4,40 @@
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
-
+    for (let i = 0; i < 21; i++) {
+  console.log(i);
+}
+for (let i = 3; i < 30; i = i + 2) {
+  console.log(i);
+}
+for (let i = 12; i >= -14; i = i - 2) {
+  console.log(i);
+}
+for (let i = 50; i >= 20; i--) {
+  if (i % 3 === 0) {
+    console.log(i);
+  }
+}
 
 
 /*Exercise #2: 
-Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].*/
 
+let str = "LaunchCode"
+let arr = [1, 5, "LC101", "blue", 42];
 
-Construct ``for`` loops to accomplish the following tasks:
+/*Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
+  for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+let reversed = "";
+for (let i = 0; i < str.length; i++) {
+  reversed = str [i] + reversed;
+  console.log(reversed);
+}
 
 
 
@@ -22,3 +45,17 @@ Construct ``for`` loops to accomplish the following tasks:
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+let arr2 = [ 2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+let evens = [];
+let odds = [];
+
+for (let i = 0; i < arr2.length; i++) {
+  if (arr2[i] % 2 === 0) {
+    evens.push(arr2[i]);
+  } else {
+    odds.push(arr2[i]);
+  }
+}
+console.log(evens);
+console.log(odds);

dc81dcf35e599cfce730e5af79fbf643e93fbda5
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-06-27 22:25:57 -0500

    Upload finished functions

--- functions/functions-exercise-1.js
+++ functions/functions-exercise-1.js
@@ -39,3 +39,34 @@ function makeDownwardStairs(height) {
     return downwardStairs.slice(0, -1);
 }
 console.log(makeDownwardStairs(5));
+
+console.log("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+
+function makeSpaceLine(numSpaces, numChars) {
+        let spaces = " ".repeat(numSpaces);
+        let chars = "#".repeat(numChars);
+    return spaces + chars + spaces;
+}
+console.log(makeSpaceLine(3, 5));
+
+console.log("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+
+function makeIsoscelesTriange(height) {
+    let triangle = "";
+    for (let i = 0; i < height; i++) {
+        triangle += (makeSpaceLine(height - i - 1, 2 * i + 1) + "\n");
+    }
+    return triangle.slice(0, -1);
+}
+console.log(makeIsoscelesTriange(5));
+
+console.log("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+
+function makeDiamond(height) {
+    let diamond = "";
+    for (let i = 0; i < height; i++) {
+        makeIsoscelesTriange;
+    }
+    return diamond.slice(0, -1);
+}
+console.log(makeDiamond(5));

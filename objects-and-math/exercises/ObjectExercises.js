249cdc12bd3684112d6dcfa8bba707b8269bbdaf
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-07-06 23:45:55 -0500

    fixed template literal

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -3,7 +3,10 @@ let superChimpOne = {
    species: "Chimpanzee",
    mass: 9,
    age: 6,
-   astronautID: 1
+   astronautID: 1,
+   move: function() {
+      return Math.floor(Math.random() * 11)
+   }
 };
 
 let salamander = {
@@ -11,7 +14,10 @@ let salamander = {
    species: "Axolotl Salamander",
    mass: 0.1,
    age: 5,
-   astronautID: 2
+   astronautID: 2,
+   move: function() {
+      return Math.floor(Math.random() * 11)
+   }
 };
 
 let superChimpTwo = {
@@ -19,7 +25,10 @@ let superChimpTwo = {
    species: "Chimpanzee",
    mass: 11,
    age: 6,
-   astronautID: 3
+   astronautID: 3,
+   move: function() {
+      return Math.floor(Math.random() * 11)
+   }
 };
 
 let dog = {
@@ -27,7 +36,10 @@ let dog = {
    species: "Beagle",
    mass: 14,
    age: 5,
-   astronautID: 4
+   astronautID: 4,
+   move: function() {
+      return Math.floor(Math.random() * 11)
+   }
 };
 
 let waterBear = {
@@ -35,11 +47,24 @@ let waterBear = {
    species: "Tardigrade",
    mass: 0.0000000001,
    age: 1,
-   astronautID: 5
-   
+   astronautID: 5,
+   move: function() {
+      return Math.floor(Math.random() * 11)
+   }
 };
 
+let crew = [superChimpOne, salamander, superChimpTwo, dog, waterBear];
 
+function crewReports() {
+   for (property in crew) { 
+      console.log(`${crew[property].name} is a ${crew[property].species}. They are ${crew[property].age} years old and ${crew[property].weight} kilograms. Their ID is ${crew[property].astronautID}.`);
+   }
+}
+crewReports();
+
+function fitnessTest(arr) {
+   
+}
 // After you have created the other object literals, add the astronautID property to each one.
 
 // Add a move method to each animal object

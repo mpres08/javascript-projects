5d434fee22fe9d086c28bdc14c879b2be0b835bb
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-07-08 21:24:45 -0500

    completed part 1

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,10 +1,23 @@
+let idNumbers = [291, 414, 503, 599, 796, 890];
+
 // Code your selectRandomEntry function here:
 
+function selectRandomEntry(arr) {
+  let randomIndex = Math.floor(Math.random()*arr.length);
+  return arr[randomIndex];
+}
 
 // Code your buildCrewArray function here:
 
-
-let idNumbers = [291, 414, 503, 599, 796, 890];
+function buildCrewArray(selectedIds, candidates) {
+  let crew = [];
+  for (let i = 0; i < candidates.length; i++) {
+    if (selectedIds.includes(candidates[i].astronautID)) {
+      crew.push(candidates[i]);
+    }
+  }
+  return crew;
+}
 
 // Here are the candidates and the 'animals' array:
 let candidateA = {
@@ -53,3 +66,17 @@ let candidateF = {
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
 // Code your template literal and console.log statements:
+
+let selectedIdNums = [];
+
+while (selectedIdNums.length < 3) {
+  let randomEntry = selectRandomEntry(idNumbers);
+  if (!selectedIdNums.includes(randomEntry)) {
+    selectedIdNums.push(randomEntry);
+  }
+}
+console.log(selectedIdNums);
+
+let selectedCrew = buildCrewArray(selectedIdNums, animals);
+
+console.log(`${selectedCrew[0].name}, ${selectedCrew[1].name}, and ${selectedCrew[2].name} are going to space!`);

a2f5a8846bb36eac52db685f76be5092c07bae61
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-08-05 21:19:53 -0500

    upload js for exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,23 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function() {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", function() {
+        missionAbort.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function() {
+        missionAbort.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", function() {
+        alert("Are you sure you want to abort the mission?");
+        paragraph.innerHTML = "Mission aborted! Space shuttle returning home."
+    });
 }
 
 window.addEventListener("load", init);
+

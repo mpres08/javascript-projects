7b7edb7697d793ae706edd97ec2e499cad505ef8
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-08-08 22:00:46 -0500

    upload bonus mission

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,6 @@
 //Code Your Solution Below
+window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+    })
+})
